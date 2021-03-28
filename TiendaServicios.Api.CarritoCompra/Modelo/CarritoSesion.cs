aecbf22a3320e2fa44e47b19cc7b7cea70a97aff
Pedro Monesterolo <peedromonesterolo@gmail.com>
2021-03-28 03:15:18 -0300

    API CARRITO COMPRAS - Desarrollo de capa CarritoCompra con BD MySql

---
+++ TiendaServicios.Api.CarritoCompra/Modelo/CarritoSesion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesion
+    {
+        public int CarritoSesionId { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+        #region RELACION CON CARRITO SESION DETALLE
+        public ICollection<CarritoSesionDetalle> ListaDetalle { get; set; } 
+        #endregion
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/Modelo/CarritoSesionDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesionDetalle
+    {
+        public int CarritoSesionDetalleId { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string ProductoSeleccionado { get; set; }
+
+        #region RELACION UNO A MUCHOS CON CARRITO SESION
+        public int CarritoSesionId { get; set; }
+        public CarritoSesion CarritoSesion { get; set; } 
+        #endregion
+    }
+}
